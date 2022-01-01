028bbd588875471df1bde6f66f794cf43133d8b8
Nassim <nacimessi10@outlook.fr>
2022-01-01 20:05:27 +0100

    Wilkinson BM25 manager

--- practice5/RunManager.py
+++ practice5/RunManager.py
@@ -32,26 +32,27 @@ class RunManager(object):
         print(len(_lines))
         self.computeLTN_LTC()
         _indexTerms = list(self.index.objects.keys())
-        # ! TODO :  Compute BM25 with Wilkinsion Methdoe
-        # _fixK1 = [(1.2, float("{:.2f}".format(_b)))
-        #           for _b in list(np.arange(0.0, 1.1, 0.1))]
-        # _fixB = [(float("{:.2f}".format(_k1)), 0.75)
-        #          for _k1 in list(np.arange(0.0, 4.2, 0.2))]
+        _fixK1 = [(1.2, float("{:.2f}".format(_b)))
+                  for _b in list(np.arange(0.0, 1.1, 0.1))]
+        _fixB = [(float("{:.2f}".format(_k1)), 0.75)
+                 for _k1 in list(np.arange(0.0, 4.2, 0.2))]
 
-        # _bm25Parameters = _fixK1 + _fixB
-        # for _paramId, _paramRun in enumerate(_bm25Parameters):
-        #     self.index.compute_bm25(k1=_paramRun[0], b=_paramRun[1])
-        #     for _line in _lines:
-        #         _topicId, *_query = _line.strip().split()
-        #         answer = self.index.compute_ranked_retrieval_as_list(
-        #             " ".join(_query))
-        #         for _i, a in enumerate(itertools.islice(answer, 1500)):
-        #             _splitedXpath = a.doc.split("/")
-        #             _f = open("{}_{}_{}_{}_title_id_categories_revision_bdy_{}_{}_k{}_b{}.txt".format(self.options.dpath+self.groupId, str(_paramId+1), "BM25",
-        #                                                                 self.granularity[1], self.stop[1] if self.options.stopwords else self.stop[0], self.stem[1] if self.options.stemmer else self.stem[0], _paramRun[0], _paramRun[1]), "a")
-        #             _f.write(_topicId+" Q0 " + _splitedXpath[0]+" "+str(_i+1)+" "+str(
-        #                 a.wtdsum)+" "+self.groupId+"BM25"+" {}\n".format("/".join(_splitedXpath[1:])))
-        #             _f.close()
+        _bm25Parameters = _fixK1 + _fixB
+        for _paramId, _paramRun in enumerate(_bm25Parameters):
+            self.index.compute_bm25(k1=_paramRun[0], b=_paramRun[1])
+            for _line in _lines:
+                self.index.RSV={}
+                
+                _topicId, *_query = _line.strip().split()
+                answer = self.index.compute_ranked_retrieval_as_list(
+                    " ".join(_query))
+                
+                for _answerDoc in answer :
+                    _ll =_answerDoc.doc.split("/")
+                    self.manageRSVDic(docParent= _answerDoc.doc, rsv=_answerDoc.wtdsum, splitedList=  _ll)
+        
+                self.sortRSVDict()
+                self.writeRunsFiles( paramId= _paramId, topicId= _topicId , param=[_paramRun[0] , _paramRun[1]]) 
 
         for _runID, _algo in enumerate(self.algorithms):
             self.index.indexMode = _algo
@@ -79,39 +80,55 @@ class RunManager(object):
                     _ll =_docNum.split("/")
                     _parentDoc=_docNum.split("/")[0]
                     
+                    self.manageRSVDic(docParent=_docNum ,rsv= _rsv,splitedList=_ll)
                     
-                    if _parentDoc in self.index.RSV:
-                        _answer :RankedRetrivialAnswerParent =self.index.RSV[_parentDoc]
-                        _answer.childrenStat.children.append({_docNum:_rsv})
-                        _answer.parentScore = _answer.parentScore + (self.getAlphaCoefself( _ll[len(_ll)-1] ) * _rsv) 
-                        self.index.RSV.update( { _parentDoc: _answer })
-                    else:
-                        _answerParent=RankedRetrivialAnswerParent()
-                        _answerParent.childrenStat=RankedRetrivialAnswer()
-                        _answerParent.childrenStat.children.append({_docNum:_rsv})
-                        _answerParent.parentScore=self.getAlphaCoefself( _ll[len(_ll)-1] ) * _rsv 
-                        self.index.RSV.update({_parentDoc: _answerParent})
                     
-                self.index.RSV=  dict(map(lambda x: self.sortedChildrenScore(x) , self.index.RSV.items() )) 
+                self.sortRSVDict()
+                self.writeRunsFiles(algo=_algo.name , paramId= _runID, topicId=_topicId)
                 
-                self.index.RSV = dict(sorted(self.index.RSV.items(), key= lambda x: self.sortedParentScore(x), reverse=True))
-                _i=1
-                for _documentRank, _doc in self.index.RSV.items():
-                    # _splitedXpath=_doc.split("/")
-                    _f = open("{}_{}_{}_{}_title_id_categories_revision_contributor_timestamp_username_bdy_{}_{}.txt".format(self.options.dpath+self.groupId, str(_runID+1), _algo.name,
-                                                                self.granularity[1], self.stop[1] if self.options.stopwords else self.stop[0], self.stem[1] if self.options.stemmer else self.stem[0]), "a")
-                    for _idc,_childy in enumerate(_doc.childrenStat.children):
-    
-                        _f.write(_topicId+" Q0 " + _documentRank+" "+str(_i)+" "+str(
-                            list(_childy.values())[0])+" "+self.groupId+_algo.name+" {}\n".format( list(_childy.keys())[0] ))
-                        _i=_i+1
-                        if _i == 1501:
-                            break
+
+    def writeRunsFiles(self, paramId, topicId, algo="BM25" , param=None ):
+        _i=1
+        _paramStr=""
+        if param:
+            _paramStr="_k{}_b{}".format(param[0], param[1])
+        for _documentRank, _doc in self.index.RSV.items():
+            _f = open("{}_{}_{}_{}_title_id_categories_revision_contributor_timestamp_username_bdy_{}_{}{}.txt".format(self.options.dpath+self.groupId, str(paramId+1), algo,
+                                                                self.granularity[1], self.stop[1] if self.options.stopwords else self.stop[0], self.stem[1] if self.options.stemmer else self.stem[0], _paramStr ), "a")
+            for _idc,_childy in enumerate(_doc.childrenStat.children):
+                
+                _f.write(topicId+" Q0 " + _documentRank+" "+str(_i)+" "+str(
+                            list(_childy.values())[0])+" "+self.groupId+algo+" {}\n".format( list(_childy.keys())[0] ))
+                _i=_i+1
+                if _i == 1501:
+                    break
                     
-                    _f.close()
-                    if _i == 1501:
-                        break    
+            _f.close()
+            if _i == 1501:
+                break
+
+    def sortRSVDict(self):
+        self.index.RSV=  dict(map(lambda x: self.sortedChildrenScore(x) , self.index.RSV.items() )) 
+                
+        self.index.RSV = dict(sorted(self.index.RSV.items(), key= lambda x: self.sortedParentScore(x), reverse=True))   
                         
+    
+    def manageRSVDic(self, docParent, splitedList , rsv ):
+        if docParent in self.index.RSV:
+            _answer :RankedRetrivialAnswerParent =self.index.RSV[splitedList[0]]
+            _answer.childrenStat.children.append({docParent:rsv})
+            _answer.parentScore = _answer.parentScore + (self.getAlphaCoefself( splitedList[len(splitedList)-1] ) * rsv) 
+            self.index.RSV.update( { splitedList[0]: _answer })
+        else:
+            _answerParent=RankedRetrivialAnswerParent()
+            _answerParent.childrenStat=RankedRetrivialAnswer()
+            _answerParent.childrenStat.children.append({docParent:rsv})
+            _answerParent.parentScore=self.getAlphaCoefself( splitedList[len(splitedList)-1] ) * rsv
+            self.index.RSV.update({splitedList[0]: _answerParent})
+                        
+        self.index.RSV=  dict(map(lambda x: self.sortedChildrenScore(x) , self.index.RSV.items() )) 
+                
+        self.index.RSV = dict(sorted(self.index.RSV.items(), key= lambda x: self.sortedParentScore(x), reverse=True))
     def sortedChildrenScore(self,x):
         _parentDoc,_answerParent=x    
         _answerParent.childrenStat.children=sorted(_answerParent.childrenStat.children , key= lambda child:list(child.values())[0], reverse=True)
